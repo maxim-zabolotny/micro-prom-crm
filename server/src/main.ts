b85c142bad18e064b8183694a4aa3016377b4d2a
Denys <zololotarenko.2015@gmail.com>
2022-06-25 16:55:50 +0300

    server: connect lib/microtron

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import microtron from 'microtron';
+console.log('microtron => ', microtron)
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
