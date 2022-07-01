abc0f9ad58ee5ca5cd685196b4c9a17a653b8a48
Denys <zololotarenko.2015@gmail.com>
2022-07-01 13:35:47 +0300

    server: change type of libs connect

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import microtron from 'microtron';
+// import microtron from '../../lib/dist/microtron/microtron.node.js';
+// console.log('microtron => ', microtron)
+
+import microtron from '@lib/microtron';
 console.log('microtron => ', microtron)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
