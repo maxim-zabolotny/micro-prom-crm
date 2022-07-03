c409dee18329a6ea18848ae87a3385d90706df3f
Denys <zololotarenko.2015@gmail.com>
2022-07-03 16:26:07 +0300

    lib: update microtron bundle

--- lib/dist/microtron/index.d.ts
+++ lib/dist/microtron/index.d.ts
@@ -2,9 +2,13 @@ import { Course } from './core/course';
 import { Category } from './core/category';
 import { Product } from './core/product';
 import { Request } from './core/request';
+import * as Types from './core/types/api';
+import * as Utils from './core/utils';
 export default class MicrotronAPI {
     static Course: typeof Course;
     static Category: typeof Category;
     static Product: typeof Product;
     static Request: typeof Request;
+    static Utils: typeof Utils;
+    static Types: typeof Types;
 }
