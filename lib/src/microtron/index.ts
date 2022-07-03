d117a09dc7e0980c54f66032034f70309db11fbf
Denys <zololotarenko.2015@gmail.com>
2022-07-03 16:25:20 +0300

    lib: expand exports from MicrotronAPI

--- lib/src/microtron/index.ts
+++ lib/src/microtron/index.ts
@@ -3,6 +3,10 @@ import { Course } from './core/course';
 import { Category } from './core/category';
 import { Product } from './core/product';
 import { Request } from './core/request';
+/*types*/
+import * as Types from './core/types/api';
+/*utils*/
+import * as Utils from './core/utils'
 /*other*/
 
 export default class MicrotronAPI {
@@ -11,4 +15,7 @@ export default class MicrotronAPI {
   public static Product = Product;
 
   public static Request = Request;
+
+  public static Utils = Utils;
+  public static Types = Types;
 }
