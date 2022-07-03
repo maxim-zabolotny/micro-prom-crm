e6b7821a4eed3e199b98a64b383eee358da9d5bf
Denys <zololotarenko.2015@gmail.com>
2022-07-03 17:04:55 +0300

    lib(microtron): add MicroError class

---
+++ lib/src/microtron/core/error/index.ts
@@ -0,0 +1,28 @@
+/*external modules*/
+import {AxiosRequestConfig} from "axios";
+/*lib*/
+/*types*/
+import {IResponseError} from "../request/IResponse";
+/*other*/
+
+export class MicroError extends Error {
+  public readonly timestamp: Date;
+  public readonly errors: string;
+
+  public readonly config: AxiosRequestConfig;
+  public readonly path: string;
+
+  constructor(response: IResponseError, config: AxiosRequestConfig, path: string) {
+    super(response.errors);
+
+    this.timestamp = response.timestamp
+    this.errors = response.errors
+
+    this.config = config;
+    this.path = path;
+  }
+
+  public static isMicroError(error: unknown): error is MicroError {
+    return error instanceof MicroError;
+  }
+}
