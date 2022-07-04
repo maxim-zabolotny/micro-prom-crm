e31af53732db263e15751c8a34ebd9b88da6018a
Denys <zololotarenko.2015@gmail.com>
2022-07-04 19:27:31 +0300

    lib(microtron): update bundle

--- lib/dist/microtron/core/utils/fromTree.d.ts
+++ lib/dist/microtron/core/utils/fromTree.d.ts
@@ -1,3 +1,3 @@
 export declare function fromTree<TEntity extends object & {
     children: TEntity[];
-}, TTree extends Array<TEntity>>(tree: TTree): TEntity[];
+}, TTree extends Array<TEntity>>(tree: TTree, idKey: string, parentIdKey: string): TEntity[];
