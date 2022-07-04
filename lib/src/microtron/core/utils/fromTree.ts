0942d88746ae63095be505171c46c6a4c5dc9046
Denys <zololotarenko.2015@gmail.com>
2022-07-04 19:27:14 +0300

    lib(microtron): update fromTree function

--- lib/src/microtron/core/utils/fromTree.ts
+++ lib/src/microtron/core/utils/fromTree.ts
@@ -5,14 +5,31 @@ import _ from 'lodash';
 export function fromTree<
   TEntity extends object & { children: TEntity[] },
   TTree extends Array<TEntity>,
->(tree: TTree): TEntity[] {
+>(tree: TTree, idKey: string, parentIdKey: string): TEntity[] {
   const nodes: TEntity[] = [];
 
   tree.forEach(
     (node: TEntity) => {
-      const localNodes: TEntity[] = [_.omit(node, 'children') as TEntity];
+      const localNodes: TEntity[] = [];
+
+      const rootNode = _.omit(node, 'children');
+      if (_.isEmpty(_.get(rootNode, parentIdKey))) {
+        _.set(rootNode, parentIdKey, 0);
+      }
+
+      localNodes.push(rootNode as TEntity);
+
       if (node.children.length > 0) {
-        localNodes.push(...fromTree(node.children) as TEntity[]);
+        localNodes.push(
+          ...fromTree(
+            _.map(node.children, (childNode) => {
+              _.set(childNode, parentIdKey, _.get(rootNode, idKey));
+              return childNode;
+            }),
+            idKey,
+            parentIdKey,
+          ) as TEntity[],
+        );
       }
 
       nodes.push(...localNodes);
