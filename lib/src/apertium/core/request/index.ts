7c36f0315f1a638ab592a4ea55439e5bd32ac35d
Denys <zololotarenko.2015@gmail.com>
2022-07-08 11:39:08 +0300

    lib(apertium): init

---
+++ lib/src/apertium/core/request/index.ts
@@ -0,0 +1,11 @@
+/*external modules*/
+/*lib*/
+/*types*/
+import { TObject } from '../types';
+/*other*/
+
+type TUnknownRec = TObject.TUnknownRec;
+
+export class Request {
+  constructor() {}
+}
---
+++ lib/src/apertium/index.ts
@@ -0,0 +1,13 @@
+/*lib*/
+import { Request } from './core/request';
+/*types*/
+import * as Types from './core/types/api';
+/*utils*/
+/*errors*/
+/*other*/
+
+export default class ApertiumAPI {
+  public static Request = Request;
+
+  public static Types = Types;
+}
