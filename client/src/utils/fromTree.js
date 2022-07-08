60b1a591c1cd4062ca69e9d6d98d34d2d734c961
Denys <zololotarenko.2015@gmail.com>
2022-07-08 15:47:34 +0300

    client: add utils

---
+++ client/src/utils/fromTree.js
@@ -0,0 +1,35 @@
+/*external modules*/
+import _ from 'lodash';
+/*other*/
+
+export function fromTree(tree, idKey, parentIdKey, transformer = (v) => v) {
+  const nodes = [];
+
+  tree.forEach((node) => {
+    const localNodes = [];
+
+    const rootNode = transformer(_.omit(node, 'children'));
+    if (_.isEmpty(_.get(rootNode, parentIdKey))) {
+      _.set(rootNode, parentIdKey, 0);
+    }
+
+    localNodes.push(rootNode);
+
+    if (node.children.length > 0) {
+      localNodes.push(
+        ...fromTree(
+          _.map(node.children, (childNode) => {
+            _.set(childNode, parentIdKey, _.get(rootNode, idKey));
+            return transformer(childNode);
+          }),
+          idKey,
+          parentIdKey
+        )
+      );
+    }
+
+    nodes.push(...localNodes);
+  });
+
+  return nodes;
+}
---
+++ client/src/utils/makeTree.js
@@ -0,0 +1,19 @@
+/*external modules*/
+import _ from 'lodash';
+/*other*/
+
+export function makeTree(nodes, key, id, transformer = (v) => v) {
+  return _.chain(nodes)
+    .filter((node) => (node[key] === id))
+    .reduce(
+      (tree, node) => [
+        ...tree,
+        {
+          ...transformer(_.omit(node, key)),
+          children: makeTree(nodes, key, _.get(node, 'id'))
+        }
+      ],
+      []
+    )
+    .value();
+}
