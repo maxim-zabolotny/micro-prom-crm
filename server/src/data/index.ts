7b05e40f04baccbfe0dcb3e193eab02cc97fe38b
Denys <zololotarenko.2015@gmail.com>
2022-07-09 18:55:44 +0300

    server(data): update namespace Data

--- server/src/data/index.ts
+++ server/src/data/index.ts
@@ -5,16 +5,17 @@ import { ICategory } from '@lib/microtron/core/category/ICategorie';
 export namespace Data {
   export namespace SelectedCategories {
     export const fileName = 'selected-categories.json';
-    export const filePath = path.join(__dirname, fileName);
+    export const filePathDist = path.join(__dirname, fileName);
+    export const filePathSRC = filePathDist.replace('dist', 'src');
 
     export async function read(): Promise<ICategory[]> {
-      const data = await fs.readFile(filePath, { encoding: 'utf-8' });
+      const data = await fs.readFile(filePathSRC, { encoding: 'utf-8' });
       return JSON.parse(data);
     }
 
     export async function write(categories: ICategory[]) {
       const data = JSON.stringify(categories, null, 2);
-      await fs.writeFile(filePath, data, { encoding: 'utf-8' });
+      await fs.writeFile(filePathSRC, data, { encoding: 'utf-8' });
     }
   }
 }
