7c9fabe1ca55d87c3b29aec5146aeff03d35241d
Denys <zololotarenko.2015@gmail.com>
2022-07-12 15:02:24 +0300

    server(auth): add JWT strategy

---
+++ server/src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,34 @@
+/*external modules*/
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+/*@interfaces*/
+import { IUserDataInJwt } from '@common/interfaces/user';
+/*@entities*/
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from '@schemas/user';
+import { Model, Types } from 'mongoose';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('jwtToken.secret'),
+    });
+  }
+
+  async validate(payload: IUserDataInJwt): Promise<UserDocument> {
+    const userId = new Types.ObjectId(payload.sub);
+
+    const user = await this.userModel.findById(userId).exec();
+    if (!user) throw new NotFoundException('User not found');
+
+    return user;
+  }
+}
