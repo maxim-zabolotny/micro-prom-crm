eead789f4e68cca826a49b97c3b1dc71b54f37b8
Denys <zololotarenko.2015@gmail.com>
2022-07-16 16:18:35 +0300

    lib(microtron): add api/ProductV2 types

---
+++ lib/src/microtron/core/types/api/ProductV2/Availability.ts
@@ -0,0 +1,6 @@
+export enum ProductAvailabilityV2 {
+  InStockUA = 'В наявності',
+  InStockRU = 'В наличии',
+  OutOfStockUA = 'Немає в наявності',
+  OutOfStockRU = 'Нет в наличии',
+}
---
+++ lib/src/microtron/core/types/api/ProductV2/index.ts
@@ -0,0 +1,1 @@
+export { ProductAvailabilityV2 } from './Availability';
