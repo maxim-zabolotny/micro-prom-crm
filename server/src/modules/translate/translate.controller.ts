1a1e4f09dac42b50e2886b92fa1a4c81912f91f0
Denys <zololotarenko.2015@gmail.com>
2022-07-23 18:54:50 +0300

    server(translate): implement translate module

--- server/src/modules/translate/translate.controller.ts
+++ server/src/modules/translate/translate.controller.ts
@@ -1,7 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { LoggingInterceptor } from '@common/interceptors';
 import { TranslateService } from './translate.service';
+import { ApertiumTranslateDto } from './dto/apertium-translate.dto';
+import { GoogleTranslateDto } from './dto/google-translate.dto';
+import { AutoTranslateDto } from './dto/auto-translate.dto';
 
 @Controller('translate')
+@UseInterceptors(LoggingInterceptor)
 export class TranslateController {
   constructor(private readonly translateService: TranslateService) {}
+
+  @Get('/detect')
+  @HttpCode(200)
+  async detectLanguage(@Query('text') text: string) {
+    const result = await this.translateService.detectLanguage(text);
+
+    return result;
+  }
+
+  @Get('/apertium')
+  @HttpCode(200)
+  async apertiumTranslate(@Query() translateData: ApertiumTranslateDto) {
+    const result = await this.translateService.translateViaApertium(
+      translateData.text,
+      translateData.from,
+      translateData.to,
+    );
+
+    return result;
+  }
+
+  @Get('/google')
+  @HttpCode(200)
+  async googleTranslate(@Query() translateData: GoogleTranslateDto) {
+    const result = await this.translateService.translateViaGoogle(
+      translateData.text,
+      translateData.from,
+      translateData.to,
+    );
+
+    return result;
+  }
+
+  @Get('/auto')
+  @HttpCode(200)
+  async autoTranslate(@Query() translateData: AutoTranslateDto) {
+    const result = await this.translateService.autoTranslate(
+      translateData.text,
+      translateData.to,
+    );
+
+    return result;
+  }
 }
--- server/src/modules/translate/translate.service.ts
+++ server/src/modules/translate/translate.service.ts
@@ -1,7 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import ApertiumAPI, {
+  Request as Apertium,
+  Types as ApertiumTypes,
+} from '@lib/apertium';
+import GoogleTranslateAPI, {
+  Request as GoogleTranslate,
+  Types as GoogleTranslateTypes,
+} from '@lib/google-translate';
 
 @Injectable()
 export class TranslateService {
-  constructor(private configService: ConfigService) {}
+  private readonly apertiumAPI: Apertium;
+  private readonly googleTranslateAPI: GoogleTranslate;
+
+  constructor(private configService: ConfigService) {
+    this.apertiumAPI = new ApertiumAPI.Request();
+
+    this.googleTranslateAPI = new GoogleTranslateAPI.Request();
+    this.googleTranslateAPI.setUseRandomUserAgent(true);
+  }
+
+  public async detectLanguage(text: string) {
+    const lang = await this.apertiumAPI.detectLanguage(text);
+    return lang;
+  }
+
+  public async translateViaApertium(
+    text: string,
+    from: ApertiumTypes.Lang,
+    to: ApertiumTypes.Lang,
+  ) {
+    const result = await this.apertiumAPI.translate(text, from, to);
+    return result;
+  }
+
+  public async translateViaGoogle(
+    text: string,
+    from: GoogleTranslateTypes.Lang,
+    to: GoogleTranslateTypes.Lang,
+  ) {
+    const result = await this.googleTranslateAPI.translate(text, from, to);
+    return result;
+  }
+
+  public async autoTranslate(text: string, to: GoogleTranslateTypes.Lang) {
+    const result = await this.translateViaGoogle(
+      text,
+      GoogleTranslateAPI.Types.Lang.Auto,
+      to,
+    );
+    return result;
+  }
 }
