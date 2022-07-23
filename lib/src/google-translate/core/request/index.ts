c9bee5f01886fcaf6f7fb18f53f850f37955aaa9
Denys <zololotarenko.2015@gmail.com>
2022-07-23 16:22:38 +0300

    lib(google-translate): add random user-agent feature

--- lib/src/google-translate/core/request/index.ts
+++ lib/src/google-translate/core/request/index.ts
@@ -6,11 +6,13 @@ import { Lang } from '../types/api';
 import { IResult } from './IResult';
 import { TTranslateResponse } from './IResponse';
 import { ITranslateRequestOptions, ITranslateRequestRawOptions } from './IRequest';
+
 /*other*/
 
 export class Request {
   protected config: AxiosRequestConfig;
   protected axios: AxiosInstance;
+  protected useRandomUserAgent = false;
 
   protected userAgent = Request.DEFAULT_HEADERS['user-agent'];
 
@@ -39,6 +41,15 @@ export class Request {
     };
   }
 
+  protected getUserAgent() {
+    if (this.useRandomUserAgent) {
+      const itemIndex = Math.floor(Math.random() * Request.userAgents.length);
+      return Request.userAgents[itemIndex];
+    }
+
+    return this.userAgent;
+  }
+
   protected parseResult(result: TTranslateResponse, targetLang: Lang): IResult {
     const [translatedText, sourceText] = result[0][0];
     const sourceLang = result[2];
@@ -63,17 +74,21 @@ export class Request {
         ...options,
       },
       headers: {
-        'user-agent': this.userAgent,
+        'user-agent': this.getUserAgent(),
       },
     });
   }
 
-  public setUserAgent(agent: string) {
+  public setDefaultUserAgent(agent: string) {
     this.userAgent = agent;
   }
 
-  public getUserAgent() {
+  public getDefaultUserAgent() {
     return this.userAgent;
+  }
+
+  public setUseRandomUserAgent(use: boolean) {
+    this.useRandomUserAgent = use;
   }
 
   public async translate(text: string, from: Lang, to: Lang) {
@@ -82,6 +97,24 @@ export class Request {
 
     return this.parseResult(data, to);
   }
+
+  public static addUserAgents(agents: string[]) {
+    this.userAgents.push(...agents);
+  }
+
+  public static removeUserAgents(agents: string[]) {
+    this.userAgents = this.userAgents.filter((agent) => !agents.includes(agent));
+  }
+
+  public static userAgents = [
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36', // 13.5%
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36', // 6.6%
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0', // 6.4%
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0', // 6.2%
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36', // 5.2%
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36', // 4.8%
+  ];
 
   public static HOST = 'https://translate.googleapis.com/translate_a/single';
   public static METHOD = 'GET';
