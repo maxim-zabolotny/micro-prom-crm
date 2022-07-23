4a00e89b4db634f98ae9271d3c1565e0bfb96659
Denys <zololotarenko.2015@gmail.com>
2022-07-23 16:50:41 +0300

    lib(google-translate): update bundle

--- lib/dist/google-translate/core/request/index.d.ts
+++ lib/dist/google-translate/core/request/index.d.ts
@@ -6,6 +6,7 @@ import { ITranslateRequestOptions, ITranslateRequestRawOptions } from './IReques
 export declare class Request {
     protected config: AxiosRequestConfig;
     protected axios: AxiosInstance;
+    protected useRandomUserAgent: boolean;
     protected userAgent: string;
     constructor({ config }?: {
         config?: AxiosRequestConfig;
@@ -13,16 +14,21 @@ export declare class Request {
     protected encodeText(text: string): string;
     protected decodeText(text: string): string;
     protected prepareRequestOptions(options: ITranslateRequestOptions): ITranslateRequestRawOptions;
+    protected getUserAgent(): string;
     protected parseResult(result: TTranslateResponse, targetLang: Lang): IResult;
     protected makeRequest(options: ITranslateRequestRawOptions): Promise<AxiosResponse<TTranslateResponse>>;
-    setUserAgent(agent: string): void;
-    getUserAgent(): string;
+    setDefaultUserAgent(agent: string): void;
+    getDefaultUserAgent(): string;
+    setUseRandomUserAgent(use: boolean): void;
     translate(text: string, from: Lang, to: Lang): Promise<IResult>;
+    static addUserAgents(agents: string[]): void;
+    static removeUserAgents(agents: string[]): void;
+    static userAgents: string[];
     static HOST: string;
     static METHOD: string;
     static DEFAULT_PARAMS: {
         client: string;
-        dt: string;
+        dt: string[];
     };
     static DEFAULT_HEADERS: {
         origin: string;
--- lib/dist/google-translate/index.d.ts
+++ lib/dist/google-translate/index.d.ts
@@ -1,5 +1,6 @@
 import { Request } from './core/request';
 import * as Types from './core/types/api';
+export { Types, Request };
 export default class GoogleTranslateAPI {
     static Request: typeof Request;
     static Types: typeof Types;
