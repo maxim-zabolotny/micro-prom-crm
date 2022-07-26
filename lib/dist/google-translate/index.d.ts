221d989c108653c119e51ce2bb6ce7198c1c744f
Denys <zololotarenko.2015@gmail.com>
2022-07-26 20:38:37 +0300

    lib(google-translate): update bundle

--- lib/dist/google-translate/index.d.ts
+++ lib/dist/google-translate/index.d.ts
@@ -2,6 +2,6 @@ import * as Request from './core/request';
 import * as Types from './core/types/api';
 export { Types, Request };
 export default class GoogleTranslateAPI {
-    static Request: typeof Request;
+    static Request: typeof Request.Request;
     static Types: typeof Types;
 }
