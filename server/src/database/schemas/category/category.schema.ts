3a62529e826e1859419f837ea8a6974ac4177556
Denys <zololotarenko.2015@gmail.com>
2022-07-31 18:38:53 +0300

    server(database): update Category entity

--- server/src/database/schemas/category/category.schema.ts
+++ server/src/database/schemas/category/category.schema.ts
@@ -1,12 +1,52 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Types } from 'mongoose';
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes } from 'mongoose';
 import { Integration } from '@schemas/integration';
 
 export type CategoryDocument = Category & Document;
 
+export type TCategoryTranslate = Pick<Category, 'name'>;
+
 @Schema({ timestamps: true, collection: 'categories' })
 export class Category {
-  @Prop({ type: Types.ObjectId, ref: 'integration', isRequired: true })
+  @Prop({ type: String, isRequired: true })
+  name: number;
+
+  @Prop({ type: Number, isRequired: true })
+  markup: number;
+
+  @Prop({ type: Number, isRequired: true })
+  course: number;
+
+  @Prop({
+    type: raw({
+      name: { type: String, isRequired: true },
+    }),
+    isRequired: true,
+  })
+  translate: TCategoryTranslate;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'categories' })
+  parent?: Category;
+
+  @Prop({ type: Boolean, isRequired: true, default: false })
+  sync: boolean;
+
+  @Prop({ type: Number })
+  promTableLine?: number;
+
+  @Prop({ type: String, isRequired: true })
+  microtronId: number;
+
+  @Prop({ type: String, isRequired: true })
+  parentMicrotronId: number;
+
+  @Prop({ type: Number, isRequired: true })
+  promId: number;
+
+  @Prop({ type: Number })
+  parentPromId?: number;
+
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'integration', isRequired: true })
   integration: Integration;
 }
 
