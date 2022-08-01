6963962c5e86190958f22ae235949d32f9cfc2ef
Denys <zololotarenko.2015@gmail.com>
2022-08-01 21:08:55 +0300

    lib(microtron): update bundle

--- lib/dist/microtron/core/utils/makeTree.d.ts
+++ lib/dist/microtron/core/utils/makeTree.d.ts
@@ -1,3 +1,3 @@
 export declare function makeTree<TEntity extends object, TKey extends keyof TEntity, TTree extends Array<Omit<TEntity, TKey> & {
     children: TEntity[];
-}>>(nodes: TEntity[], key: TKey, id: number): TTree;
+}>>(nodes: TEntity[], key: TKey, id: number | string): TTree;
