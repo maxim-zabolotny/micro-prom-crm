e2ec757961b20b071fe7d9c896bfa4b7233db06a
Denys <zololotarenko.2015@gmail.com>
2022-08-02 13:56:07 +0300

    server(admin/sync-prom): fix controller

--- server/src/modules/admin/sync-prom/sync-prom.controller.ts
+++ server/src/modules/admin/sync-prom/sync-prom.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Get, HttpCode, UseInterceptors } from '@nestjs/common';
 import { LoggingInterceptor } from '@common/interceptors';
 import { SyncPromService } from './sync-prom.service';
 
-@Controller('/admin/sync/local')
+@Controller('/admin/sync/prom')
 @UseInterceptors(LoggingInterceptor)
 export class SyncPromController {
-  constructor(private readonly syncLocalService: SyncPromService) {}
+  constructor(private readonly syncPromService: SyncPromService) {}
 
   @Get('/load-all-categories')
   @HttpCode(200)
   // @Auth(UserRole.Admin)
   async loadAllCategories() {
-    return {};
+    return this.syncPromService.loadAllCategories();
   }
 
   @Get('/sync-all-categories')
