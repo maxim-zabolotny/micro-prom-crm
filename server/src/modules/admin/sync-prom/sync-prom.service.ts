0142d6ecb66d48063654bda567e13bcfbdfb3b54
Denys <zololotarenko.2015@gmail.com>
2022-08-02 20:27:10 +0300

    server(admin/sync-prom): update SyncProm service with Spreadsheet service

--- server/src/modules/admin/sync-prom/sync-prom.service.ts
+++ server/src/modules/admin/sync-prom/sync-prom.service.ts
@@ -1,95 +1,21 @@
 import * as _ from 'lodash';
-import {
-  GoogleSpreadsheet,
-  GoogleSpreadsheetWorksheet,
-} from 'google-spreadsheet';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Model, Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from '@schemas/category';
 import { AppConstants } from '../../../app.constants';
+import { SpreadsheetService } from '../../spreadsheet/spreadsheet.service';
 
 @Injectable()
 export class SyncPromService {
   private readonly logger = new Logger(this.constructor.name);
 
-  private doc!: GoogleSpreadsheet;
-  private isAuthenticated = false;
-
   constructor(
     private configService: ConfigService,
+    private spreadsheetService: SpreadsheetService,
     @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
   ) {}
-
-  private async authDoc() {
-    if (!this.isAuthenticated) {
-      this.doc = new GoogleSpreadsheet(AppConstants.Google.Sheet.ID);
-
-      this.logger.debug('Auth in Google Sheet');
-      await this.doc.useServiceAccountAuth({
-        client_email: this.configService.get('google.serviceAccountEmail'),
-        private_key: this.configService
-          .get('google.privateKey')
-          .replaceAll('\\n', '\n'),
-      });
-
-      this.logger.debug('Load Google Sheet info');
-      await this.doc.loadInfo();
-
-      this.isAuthenticated = true;
-
-      return {
-        productsSheet: this.doc.sheetsByIndex[0],
-        groupsSheet: this.doc.sheetsByIndex[1],
-      };
-    }
-
-    return {
-      productsSheet: this.doc.sheetsByIndex[0],
-      groupsSheet: this.doc.sheetsByIndex[1],
-    };
-  }
-
-  private async findRowBy(
-    sheet: GoogleSpreadsheetWorksheet,
-    data: Record<string, string | number>,
-  ) {
-    const limit = 20;
-    const countOfRows = sheet.rowCount;
-
-    this.logger.debug('Find row by:', {
-      limit,
-      countOfRows,
-      data,
-    });
-
-    let offset = 0;
-    while (offset <= countOfRows) {
-      this.logger.debug('Request rows:', {
-        limit,
-        offset,
-      });
-
-      const rows = await sheet.getRows({ limit, offset });
-      if (_.isEmpty(rows)) {
-        this.logger.debug('Target row not found. No more rows');
-        break;
-      }
-
-      const targetRow = _.find(rows, (row) => {
-        return _.every(data, (value, key) => {
-          return row[key] === String(value);
-        });
-      });
-      if (targetRow) return targetRow;
-
-      this.logger.debug('Target row not found. Continue');
-      offset += limit;
-    }
-
-    return null;
-  }
 
   private mapEntityFields(mapRecord: string[][], entity: Record<string, any>) {
     return Object.fromEntries(
@@ -99,18 +25,8 @@ export class SyncPromService {
     );
   }
 
-  public async getCategoriesSheet() {
-    const { groupsSheet } = await this.authDoc();
-    return groupsSheet;
-  }
-
-  public async getProductsSheet() {
-    const { productsSheet } = await this.authDoc();
-    return productsSheet;
-  }
-
   public async loadAllCategories() {
-    const categoriesSheet = await this.getCategoriesSheet();
+    const categoriesSheet = await this.spreadsheetService.getCategoriesSheet();
 
     const limit = 50;
     const result = {
