21ac075650aa6566f1b6fb5e528365c8cf97d27c
Denys <zololotarenko.2015@gmail.com>
2022-08-02 18:39:19 +0300

    server: add Prom namespace to App constants

--- server/src/app.constants.ts
+++ server/src/app.constants.ts
@@ -10,4 +10,17 @@ export namespace AppConstants {
       export const ID = '118REcDeqq0oABCp4AuAeHPAD5otXtk4HtKrOI5cvljI';
     }
   }
+
+  export namespace Prom {
+    export namespace Sheet {
+      export const CategoryFieldsMapping = [
+        ['Номер_группы', 'promId'],
+        ['Название_группы', 'translate.name'],
+        ['Название_группы_укр', 'name'],
+        ['Идентификатор_группы', '_id'],
+        ['Номер_родителя', 'parentPromId'],
+        ['Идентификатор_родителя', 'parent._id'],
+      ];
+    }
+  }
 }
