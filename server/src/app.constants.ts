a3be96ed3cc8e3ea8d83418314d01ef198918bcf
Denys <zololotarenko.2015@gmail.com>
2022-08-02 16:13:49 +0300

    server: update App constants

--- server/src/app.constants.ts
+++ server/src/app.constants.ts
@@ -2,4 +2,12 @@ export namespace AppConstants {
   export namespace Product {
     export const changeFields = [];
   }
+
+  export namespace Google {
+    export namespace Sheet {
+      export const URL =
+        'https://docs.google.com/spreadsheets/d/118REcDeqq0oABCp4AuAeHPAD5otXtk4HtKrOI5cvljI';
+      export const ID = '118REcDeqq0oABCp4AuAeHPAD5otXtk4HtKrOI5cvljI';
+    }
+  }
 }
