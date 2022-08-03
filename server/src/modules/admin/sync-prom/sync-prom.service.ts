509a201fc90844bf25911a52d94a0e9e9da47191
Denys <zololotarenko.2015@gmail.com>
2022-08-03 14:00:23 +0300

    server(admin/sync-prom): add new methods to service and implement syncAllCategories method

--- server/src/modules/admin/sync-prom/sync-prom.service.ts
+++ server/src/modules/admin/sync-prom/sync-prom.service.ts
@@ -1,11 +1,17 @@
 import * as _ from 'lodash';
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Model, Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from '@schemas/category';
 import { AppConstants } from '../../../app.constants';
 import { SpreadsheetService } from '../../spreadsheet/spreadsheet.service';
+import { DataUtilsHelper } from '@common/helpers';
+
+export interface IDeletedCategoryInfo {
+  id: string;
+  rowIndex: number;
+}
 
 @Injectable()
 export class SyncPromService {
@@ -14,6 +20,7 @@ export class SyncPromService {
   constructor(
     private configService: ConfigService,
     private spreadsheetService: SpreadsheetService,
+    private dataUtilsHelper: DataUtilsHelper,
     @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
   ) {}
 
@@ -25,9 +32,107 @@ export class SyncPromService {
     );
   }
 
-  public async loadAllCategories() {
-    const categoriesSheet = await this.spreadsheetService.getCategoriesSheet();
+  public async getCountOfNewCategories() {
+    return this.categoryModel.count({ sync: false });
+  }
 
+  public async getDeletedCategories(
+    idKey: string,
+  ): Promise<IDeletedCategoryInfo[]> {
+    const categoriesSheet = this.spreadsheetService.getCategoriesSheet();
+
+    this.logger.debug('Load Category ids from Google Sheet');
+    const allRows = await this.spreadsheetService.getAllRows(categoriesSheet, [
+      idKey,
+      'rowIndex',
+    ]);
+    const categoryIdsInSheet = _.map(allRows, idKey);
+
+    this.logger.debug(
+      'Load Category ids from DB by Category ids from Google Sheet',
+    );
+    const categories = await this.categoryModel
+      .find({
+        _id: { $in: categoryIdsInSheet },
+      })
+      .select({
+        _id: 1,
+      });
+    const categoryIdsInDB = _.map(categories, (category) =>
+      category._id.toString(),
+    );
+
+    const { removed: removedIds } = this.dataUtilsHelper.getDiff(
+      categoryIdsInDB,
+      categoryIdsInSheet,
+    );
+    this.logger.debug('Detected removed Category ids:', {
+      removedIds,
+    });
+
+    return _.chain(allRows)
+      .filter((row) => _.includes(removedIds, row[idKey]))
+      .map((row) => ({ id: row[idKey], rowIndex: Number(row.rowIndex) }))
+      .value();
+  }
+
+  public async deleteCategoriesFromSheet(
+    categories: IDeletedCategoryInfo[],
+    idKey: string,
+  ) {
+    const categoriesSheet = this.spreadsheetService.getCategoriesSheet();
+
+    this.logger.debug('Remove Categories from Google Sheet');
+
+    const removedCategoriesFromSheet = await Promise.all(
+      _.map(categories, (category) => {
+        return this.spreadsheetService.removeOneRowBy(
+          categoriesSheet,
+          {
+            [idKey]: category.id,
+          },
+          this.spreadsheetService.getPaginationForRowIndex(category.rowIndex),
+        );
+      }),
+    );
+    this.logger.debug('Removed Categories from Google Sheet:', {
+      categories: _.map(removedCategoriesFromSheet, (category) => ({
+        id: category['Идентификатор_группы'],
+        name: category['Название_группы'],
+      })),
+    });
+
+    return removedCategoriesFromSheet;
+  }
+
+  public async getNewCategories(offset: number, limit: number) {
+    return this.categoryModel.find({ sync: false }).skip(offset).limit(limit);
+  }
+
+  public async addCategoriesToSheet(categories: Category[]) {
+    const categoriesSheet = this.spreadsheetService.getCategoriesSheet();
+
+    this.logger.debug('Build bulk rows for Google Sheet');
+    const bulkRows = _.map(categories, (category) =>
+      this.mapEntityFields(
+        AppConstants.Prom.Sheet.CategoryFieldsMapping,
+        category,
+      ),
+    );
+
+    this.logger.debug('Add rows with Categories to Google Sheet:', {
+      count: bulkRows.length,
+    });
+
+    const addedRows = await categoriesSheet.addRows(bulkRows);
+    this.logger.debug('Added rows to Google Sheet:', {
+      count: addedRows.length,
+    });
+
+    return addedRows;
+  }
+
+  public async loadAllNewCategories() {
     const limit = 50;
     const result = {
       count: 0,
@@ -36,7 +141,7 @@ export class SyncPromService {
       success: true,
     };
 
-    const count = await this.categoryModel.count({ sync: false });
+    const count = await this.getCountOfNewCategories();
     this.logger.debug('Not synced Categories count in DB', { count });
     if (count === 0) {
       return result;
@@ -53,31 +158,13 @@ export class SyncPromService {
         offset,
       });
 
-      const categories = await this.categoryModel
-        .find({ sync: false })
-        .skip(offset)
-        .limit(limit);
+      const categories = await this.getNewCategories(offset, limit);
       this.logger.debug('Loaded not synced Categories:', {
         count: categories.length,
       });
 
       // ADD TO SHEET
-      this.logger.debug('Build bulk rows for Google Sheet');
-      const bulkRows = _.map(categories, (category) =>
-        this.mapEntityFields(
-          AppConstants.Prom.Sheet.CategoryFieldsMapping,
-          category,
-        ),
-      );
-
-      this.logger.debug('Add rows with Categories to Google Sheet:', {
-        count: bulkRows.length,
-      });
-
-      const addedRows = await categoriesSheet.addRows(bulkRows);
-      this.logger.debug('Added rows to Google Sheet:', {
-        count: addedRows.length,
-      });
+      const addedRows = await this.addCategoriesToSheet(categories);
 
       // RESULT
       result.added += addedRows.length;
@@ -125,5 +212,49 @@ export class SyncPromService {
       ...result,
       success,
     };
+  }
+
+  public async syncAllCategories(add = true, remove = true) {
+    if (!add && !remove) {
+      throw new HttpException('Nothing for to do', HttpStatus.BAD_REQUEST);
+    }
+
+    this.logger.debug('Sync Prom actions:', {
+      add,
+      remove,
+    });
+
+    const result = {
+      removed: 0,
+      added: 0,
+      updated: 0,
+    };
+
+    if (remove) {
+      // SELECT
+      const idKey = 'Идентификатор_группы';
+      const deletedCategories = await this.getDeletedCategories(idKey);
+
+      // REMOVE
+      if (!_.isEmpty(deletedCategories)) {
+        const removedCategoriesFromSheet = await this.deleteCategoriesFromSheet(
+          deletedCategories,
+          idKey,
+        );
+
+        // RESULT
+        result.removed = removedCategoriesFromSheet.length;
+      }
+    }
+
+    if (add) {
+      const { added, updated } = await this.loadAllNewCategories();
+
+      // RESULT
+      result.added = added;
+      result.updated = updated;
+    }
+
+    return result;
   }
 }
