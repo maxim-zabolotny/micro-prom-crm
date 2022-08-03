ad6930a2e621ab04255f9a56fea919f241af58bf
Denys <zololotarenko.2015@gmail.com>
2022-08-03 16:02:08 +0300

    server(admin): optimize Admin imports; update SyncLocal service

--- server/src/modules/admin/admin.module.ts
+++ server/src/modules/admin/admin.module.ts
@@ -4,48 +4,24 @@ import { AdminService } from './admin.service';
 import { SyncLocalController } from './sync-local/sync-local.controller';
 import { SyncLocalService } from './sync-local/sync-local.service';
 import { MicrotronModule } from '../microtron/microtron.module';
-import {
-  DataGenerateHelper,
-  DataUtilsHelper,
-  TimeHelper,
-} from '@common/helpers';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Constant, ConstantSchema } from '@schemas/constant';
-import { Category, CategorySchema } from '@schemas/category';
 import { Integration, IntegrationSchema } from '@schemas/integration';
 import { SyncPromController } from './sync-prom/sync-prom.controller';
 import { SyncPromService } from './sync-prom/sync-prom.service';
+import { CrmModule } from '../crm/crm.module';
 
 @Module({
   imports: [
     MicrotronModule,
-    MongooseModule.forFeature([
-      {
-        name: Constant.name,
-        schema: ConstantSchema,
-      },
-    ]),
+    CrmModule,
     MongooseModule.forFeature([
       {
         name: Integration.name,
         schema: IntegrationSchema,
       },
     ]),
-    MongooseModule.forFeature([
-      {
-        name: Category.name,
-        schema: CategorySchema,
-      },
-    ]),
   ],
   controllers: [AdminController, SyncLocalController, SyncPromController],
-  providers: [
-    DataUtilsHelper,
-    DataGenerateHelper,
-    TimeHelper,
-    AdminService,
-    SyncLocalService,
-    SyncPromService,
-  ],
+  providers: [AdminService, SyncLocalService, SyncPromService],
 })
 export class AdminModule {}
--- server/src/modules/admin/sync-local/sync-local.service.ts
+++ server/src/modules/admin/sync-local/sync-local.service.ts
@@ -1,17 +1,11 @@
 import * as _ from 'lodash';
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Constant, ConstantDocument } from '@schemas/constant';
 import { Model, Types } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import {
-  DataGenerateHelper,
-  DataUtilsHelper,
-  TimeHelper,
-} from '@common/helpers';
-import { Category, CategoryDocument } from '@schemas/category';
-import { CoursesService } from '../../microtron/courses/courses.service';
-import { CategoriesService } from '../../microtron/categories/categories.service';
+import { CategoryDocument } from '@schemas/category';
+import { MicrotronCoursesService } from '../../microtron/courses/courses.service';
+import { MicrotronCategoriesService } from '../../microtron/categories/categories.service';
 import { ICategoryInConstant } from '@common/interfaces/category';
 import MicrotronAPI from '@lib/microtron';
 import {
@@ -19,6 +13,7 @@ import {
   IntegrationCompany,
   IntegrationDocument,
 } from '@schemas/integration';
+import { CrmCategoriesService } from '../../crm/categories/categories.service';
 
 export interface ITranslatedCategoryInConstant extends ICategoryInConstant {
   ruName: string;
@@ -43,69 +38,12 @@ export class SyncLocalService {
 
   constructor(
     private configService: ConfigService,
-    private coursesService: CoursesService,
-    private categoriesService: CategoriesService,
-    private dataUtilHelper: DataUtilsHelper,
-    private dataGenerateHelper: DataGenerateHelper,
-    private timeHelper: TimeHelper,
-    @InjectModel(Constant.name) private constantModel: Model<ConstantDocument>,
-    @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
+    private microtronCoursesService: MicrotronCoursesService,
+    private microtronCategoriesService: MicrotronCategoriesService,
+    private crmCategoriesService: CrmCategoriesService,
     @InjectModel(Integration.name)
     private integrationModel: Model<IntegrationDocument>,
   ) {}
-
-  // TODO: Move to categories CRM
-  public async addCategory(categoryData: TAddCategory) {
-    this.logger.debug('Process category:', {
-      id: categoryData.id,
-      name: categoryData.name,
-    });
-
-    const category = new this.categoryModel({
-      name: categoryData.name,
-      markup: categoryData.markup,
-      course: categoryData.course,
-      translate: {
-        name: categoryData.ruName,
-      },
-      parent: categoryData.parent?._id,
-      microtronId: categoryData.id,
-      parentMicrotronId: categoryData.parent?.microtronId,
-      promId: this.dataGenerateHelper.randomNumber(1, 9, 8),
-      parentPromId: categoryData.parent?.promId,
-      integration: categoryData.integrationId,
-    });
-    await category.save();
-
-    const children = [];
-    if (!_.isEmpty(categoryData.children)) {
-      this.logger.debug('Process category children');
-
-      children.push(
-        ...(await Promise.all(
-          _.map(categoryData.children, (categoryChildren) => {
-            return this.addCategory({
-              ...categoryChildren,
-              ..._.pick(categoryData, ['course', 'integrationId']),
-              parent: category,
-            });
-          }),
-        )),
-      );
-    }
-
-    return {
-      ...(category.toJSON() as Category),
-      children,
-    };
-  }
-
-  public async updateCategory(
-    categoryId: Types.ObjectId,
-    data: TUpdateCategory,
-  ) {}
-
-  public async deleteCategory(categoryId: Types.ObjectId) {}
 
   public async loadAllCategories() {
     this.logger.debug('Load Microtron Integration from DB');
@@ -120,14 +58,15 @@ export class SyncLocalService {
       );
     }
 
-    const course = await this.coursesService.getCoursesByAPI(false);
+    const course = await this.microtronCoursesService.getCoursesByAPI(false);
 
-    const uaCategories = (await this.categoriesService.getSaved(
+    const uaCategories = (await this.microtronCategoriesService.getSaved(
       false,
     )) as ICategoryInConstant[];
-    const ruCategories = (await this.categoriesService.getSavedRUTranslate(
-      false,
-    )) as ICategoryInConstant[];
+    const ruCategories =
+      (await this.microtronCategoriesService.getSavedRUTranslate(
+        false,
+      )) as ICategoryInConstant[];
 
     this.logger.debug('Mapping UA and RU categories');
     const categories: ITranslatedCategoryInConstant[] = _.map(
@@ -154,14 +93,20 @@ export class SyncLocalService {
     ) as unknown as ITranslatedCategoryTreeInConstant[];
 
     this.logger.debug('Start loading categories to DB');
-    return await Promise.all(
+
+    const addedCategories = await Promise.all(
       _.map(categoriesTree, (categoryData) => {
-        return this.addCategory({
+        return this.crmCategoriesService.addCategoryToDB({
           ...categoryData,
           course: course.bank,
           integrationId: microtronIntegration._id,
         });
       }),
     );
+    this.logger.debug('Loaded categories to DB:', {
+      count: addedCategories.length,
+    });
+
+    return addedCategories;
   }
 }
