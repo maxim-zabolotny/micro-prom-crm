0f3379ec4bc205f13d70b483f6b9d5e61b6ae65f
Denys <zololotarenko.2015@gmail.com>
2022-08-03 14:08:01 +0300

    server(crm): init CRM module

---
+++ server/src/modules/crm/crm.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CrmController } from './crm.controller';
+import { CrmService } from './crm.service';
+
+@Module({
+  imports: [],
+  controllers: [CrmController],
+  providers: [CrmService],
+})
+export class CrmModule {}
