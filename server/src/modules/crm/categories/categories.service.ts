2995cbe576c24240a0873bc80c914e5d1dece038
Denys <zololotarenko.2015@gmail.com>
2022-08-03 15:59:23 +0300

    server(crm): update Categories service; update CRM module

--- server/src/modules/crm/categories/categories.service.ts
+++ server/src/modules/crm/categories/categories.service.ts
@@ -1,9 +1,74 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Category, CategoryDocument } from '@schemas/category';
+import * as _ from 'lodash';
+import { Model, Types } from 'mongoose';
+import {
+  TAddCategory,
+  TUpdateCategory,
+} from '../../admin/sync-local/sync-local.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { DataGenerateHelper } from '@common/helpers';
 
 @Injectable()
 export class CrmCategoriesService {
   private readonly logger = new Logger(this.constructor.name);
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    private dataGenerateHelper: DataGenerateHelper,
+    @InjectModel(Category.name) private categoryModel: Model<CategoryDocument>,
+  ) {}
+
+  public async addCategoryToDB(categoryData: TAddCategory) {
+    this.logger.debug('Process category:', {
+      id: categoryData.id,
+      name: categoryData.name,
+    });
+
+    const category = new this.categoryModel({
+      name: categoryData.name,
+      markup: categoryData.markup,
+      course: categoryData.course,
+      translate: {
+        name: categoryData.ruName,
+      },
+      parent: categoryData.parent?._id,
+      microtronId: categoryData.id,
+      parentMicrotronId: categoryData.parent?.microtronId,
+      promId: this.dataGenerateHelper.randomNumber(1, 9, 8),
+      parentPromId: categoryData.parent?.promId,
+      integration: categoryData.integrationId,
+    });
+    await category.save();
+
+    const children: Category[] = [];
+    if (!_.isEmpty(categoryData.children)) {
+      this.logger.debug('Process category children');
+
+      children.push(
+        ...(await Promise.all(
+          _.map(categoryData.children, (categoryChildren) => {
+            return this.addCategoryToDB({
+              ...categoryChildren,
+              ..._.pick(categoryData, ['course', 'integrationId']),
+              parent: category,
+            });
+          }),
+        )),
+      );
+    }
+
+    return {
+      ...(category.toJSON() as Category),
+      children,
+    };
+  }
+
+  public async updateCategoryInDB(
+    categoryId: Types.ObjectId,
+    data: TUpdateCategory,
+  ) {}
+
+  public async deleteCategoryFromDB(categoryId: Types.ObjectId) {}
 }
--- server/src/modules/crm/crm.module.ts
+++ server/src/modules/crm/crm.module.ts
@@ -3,10 +3,21 @@ import { CrmController } from './crm.controller';
 import { CrmService } from './crm.service';
 import { CrmCategoriesController } from './categories/categories.controller';
 import { CrmCategoriesService } from './categories/categories.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from '@schemas/category';
+import { DataGenerateHelper } from '@common/helpers';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Category.name,
+        schema: CategorySchema,
+      },
+    ]),
+  ],
   controllers: [CrmController, CrmCategoriesController],
-  providers: [CrmService, CrmCategoriesService],
+  providers: [DataGenerateHelper, CrmService, CrmCategoriesService],
+  exports: [CrmCategoriesService],
 })
 export class CrmModule {}
