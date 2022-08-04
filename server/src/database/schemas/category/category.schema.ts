9a400c164ba7a62fc5a01d73769bfc7e0e7fe799
Denys <zololotarenko.2015@gmail.com>
2022-08-04 22:00:53 +0300

    server(database/category): remove uniq constraint from Category name

--- server/src/database/schemas/category/category.schema.ts
+++ server/src/database/schemas/category/category.schema.ts
@@ -8,7 +8,7 @@ export type TCategoryTranslate = Pick<Category, 'name'>;
 
 @Schema({ timestamps: true, collection: 'categories' })
 export class Category {
-  @Prop({ type: String, required: true, unique: true })
+  @Prop({ type: String, required: true })
   name: string;
 
   @Prop({ type: Number, required: true })
