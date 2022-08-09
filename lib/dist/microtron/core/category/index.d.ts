37ba4113818fcfa4419af47c74d3a6a999e072a0
Denys <zololotarenko.2015@gmail.com>
2022-08-09 17:21:50 +0300

    lib(microtron): update bundle

--- lib/dist/microtron/core/category/index.d.ts
+++ lib/dist/microtron/core/category/index.d.ts
@@ -1,15 +1,14 @@
 import { Request } from '../request';
-import { TEntity as TProductEntity } from '../product';
+import { IProductRequestOptions, TEntity as TProductEntity } from '../product';
 import { Lang } from '../types/api';
 import { ICategoriesTree, ICategory, ICategoryRaw } from './ICategorie';
 import { IResponseRaw } from '../request/IResponse';
-import { IProductRequestOptions } from '../product/IOptions';
 export declare type TEntity = ICategory;
 export declare type TRawEntity = ICategoryRaw;
 export { ICategory, ICategoryRaw, ICategoriesTree, };
 export declare class Category extends Request<TEntity[], TRawEntity[]> {
     protected parseResult(data: IResponseRaw<TRawEntity[]>): {
-        data: ICategoryRaw[];
+        data: ICategory[];
         timestamp: Date;
         status: boolean;
     };
