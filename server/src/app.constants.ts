2bc381b75ff447dc642b32786e9f52820f4f74a0
Denys <zololotarenko.2015@gmail.com>
2022-08-10 16:01:53 +0300

    server: update app constants

--- server/src/app.constants.ts
+++ server/src/app.constants.ts
@@ -26,16 +26,24 @@ export namespace AppConstants {
           ['Идентификатор_группы', '_id', returnTheSame],
           ['Номер_родителя', 'parentPromId', returnTheSame],
           ['Идентификатор_родителя', 'parent', returnTheSame],
-          ['HTML_заголовок_группы', 'translate.name', returnTheSame],
-          ['HTML_заголовок_группы_укр', 'name', returnTheSame],
-          ['HTML_ключевые_слова_группы', 'translate.name', returnTheSame],
-          ['HTML_ключевые_слова_группы_укр', 'name', returnTheSame],
+          // ['HTML_заголовок_группы', 'translate.name', returnTheSame],
+          // ['HTML_заголовок_группы_укр', 'name', returnTheSame],
+          // ['HTML_ключевые_слова_группы', 'translate.name', returnTheSame],
+          // ['HTML_ключевые_слова_группы_укр', 'name', returnTheSame],
         ];
 
         export const FieldsMappingDefaults: TFieldsMappingDefaults = [];
       }
 
       export namespace Product {
+        export const START_PROPERTIES_CELL_INDEX = 47;
+
+        export const AVAILABLE_NUMBER_OF_PROPERTIES = 50;
+        export const SINGLE_PROPERTY_CELLS_SIZE = 3;
+
+        export const NUMBER_OF_CELLS_FOR_PROPERTIES =
+          AVAILABLE_NUMBER_OF_PROPERTIES * SINGLE_PROPERTY_CELLS_SIZE;
+
         export const FieldsMapping: TFieldsMapping = [
           ['Код_товара', 'promId', returnTheSame],
           ['Название_позиции', 'translate.name', returnTheSame],
@@ -53,21 +61,23 @@ export namespace AppConstants {
           ['Наличие', 'available', (v: boolean) => (v ? '"+"' : '"-"')],
           ['Количество', 'quantity', returnTheSame],
           ['Производитель', 'brand', returnTheSame],
-          ['Номер_группы', 'category.microtronId', returnTheSame],
+          ['Номер_группы', 'promGroupNumber', returnTheSame],
           ['Идентификатор_товара', '_id', returnTheSame],
-          ['HTML_заголовок', 'translate.name', returnTheSame],
-          ['HTML_заголовок_укр', 'name', returnTheSame],
-          ['HTML_описание', 'translate.description', returnTheSame],
-          ['HTML_описание_укр', 'description', returnTheSame],
-          ['HTML_ключевые_слова', 'translate.name', returnTheSame],
-          ['HTML_ключевые_слова_укр', 'name', returnTheSame],
+          // ['HTML_заголовок', 'translate.name', returnTheSame],
+          // ['HTML_заголовок_укр', 'name', returnTheSame],
+          // ['HTML_описание', 'translate.description', returnTheSame],
+          // ['HTML_описание_укр', 'description', returnTheSame],
+          // ['HTML_ключевые_слова', 'translate.name', returnTheSame],
+          // ['HTML_ключевые_слова_укр', 'name', returnTheSame],
         ];
 
         export const FieldsMappingDefaults: TFieldsMappingDefaults = [
           ['Тип_товара', 'r'],
           ['Валюта', 'UAH'],
           ['Единица_измерения', 'шт'],
-          ['Где_находится_товар', 'Запоріжжя'],
+          ['Минимальный_объем_заказа', '1'],
+          ['Цена от', '"-"'],
+          ['Где_находится_товар', 'Запоріжжя'], // 194007000 - Запоріжжя - Запорожье
         ];
       }
     }
