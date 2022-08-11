265ce8e613bd01054351c3ba674f3fe79e77341b
Denys <zololotarenko.2015@gmail.com>
2022-08-11 19:37:02 +0300

    lib(prom): add PaymentOption class

---
+++ lib/src/prom/core/paymentOption/index.ts
@@ -0,0 +1,32 @@
+/*external modules*/
+import urlJoin from 'url-join';
+/*lib*/
+import { HttpMethods, Request } from '../request';
+/*types*/
+import { IPaymentOption, TGetPaymentOptionsListResponse } from './IPaymentOption';
+/*other*/
+
+export {
+  IPaymentOption,
+
+  TGetPaymentOptionsListResponse,
+};
+
+export class PaymentOption extends Request {
+  protected buildUrl(path: string | number): string {
+    return urlJoin(PaymentOption.BASE_PATH, String(path));
+  }
+
+  public async getPaymentOptionsList(): Promise<TGetPaymentOptionsListResponse> {
+    const { body } = await this.makeRequest<{}, {}, TGetPaymentOptionsListResponse>(
+      HttpMethods.Get,
+      this.buildUrl('list'),
+      {},
+      {},
+    );
+
+    return body;
+  }
+
+  public static readonly BASE_PATH = 'payment_options';
+}
