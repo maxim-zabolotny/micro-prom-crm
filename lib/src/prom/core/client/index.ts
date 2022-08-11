6b5e09f6d3927bd8d9f4003c4cb05d4b7ab64899
Denys <zololotarenko.2015@gmail.com>
2022-08-11 19:25:11 +0300

    lib(prom): add Client class

---
+++ lib/src/prom/core/client/index.ts
@@ -0,0 +1,50 @@
+/*external modules*/
+import urlJoin from 'url-join';
+/*lib*/
+import { HttpMethods, Request } from '../request';
+/*types*/
+import {
+  IClient, IGetClientsListQueryParams, TGetClientByIdResponse, TGetClientsListResponse,
+} from './IClient';
+/*other*/
+
+export {
+  IClient,
+
+  IGetClientsListQueryParams,
+
+  TGetClientsListResponse,
+  TGetClientByIdResponse,
+};
+
+export class Client extends Request {
+  protected buildUrl(path: string | number): string {
+    return urlJoin(Client.BASE_PATH, String(path));
+  }
+
+  public async getClientsList(params: IGetClientsListQueryParams = {}): Promise<TGetClientsListResponse> {
+    const queryParams = { ...params };
+
+    const { body } = await this.makeRequest<{}, IGetClientsListQueryParams, TGetClientsListResponse>(
+      HttpMethods.Get,
+      this.buildUrl('list'),
+      {},
+      queryParams,
+    );
+
+    return body;
+  }
+
+  public async getClientById(clientId: number): Promise<TGetClientByIdResponse> {
+    const { body } = await this.makeRequest<{}, {}, TGetClientByIdResponse>(
+      HttpMethods.Get,
+      this.buildUrl(clientId),
+      {},
+      {},
+    );
+
+    return body;
+  }
+
+  public static readonly BASE_PATH = 'clients';
+}
