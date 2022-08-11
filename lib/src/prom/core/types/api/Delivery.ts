3addd1033ecc064fe38a2d52cd5ba5a05bc8ad86
Denys <zololotarenko.2015@gmail.com>
2022-08-11 19:42:35 +0300

    lib(prom): add Delivery enums

---
+++ lib/src/prom/core/types/api/Delivery.ts
@@ -0,0 +1,14 @@
+export enum DeliveryProvider {
+  NovaPoshta = 'nova_poshta',
+  Justin = 'justin',
+  DeliveryAuto = 'delivery_auto',
+  UkrPoshta = 'ukrposhta',
+}
+
+//  W - отделение, D - курьер
+export enum DeliveryType {
+  W2W = 'W2W',
+  W2D = 'W2D',
+  D2W = 'D2W',
+  D2D = 'D2D'
+}
