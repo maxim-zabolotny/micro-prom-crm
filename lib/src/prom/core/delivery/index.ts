258d61cd6dbef9ee6b1e53425c897d505417c1f0
Denys <zololotarenko.2015@gmail.com>
2022-08-11 19:53:56 +0300

    lib(prom): add Delivery class

---
+++ lib/src/prom/core/delivery/index.ts
@@ -0,0 +1,34 @@
+/*external modules*/
+import urlJoin from 'url-join';
+/*lib*/
+import { HttpMethods, Request } from '../request';
+/*types*/
+import { IPostSaveDeliveryDeclarationBody, TPostSaveDeliveryDeclarationResponse } from './IDelivery';
+/*other*/
+
+export {
+  IPostSaveDeliveryDeclarationBody,
+
+  TPostSaveDeliveryDeclarationResponse,
+};
+
+export class Delivery extends Request {
+  protected buildUrl(path: string | number): string {
+    return urlJoin(Delivery.BASE_PATH, String(path));
+  }
+
+  public async saveDeliveryDeclaration(
+    data: IPostSaveDeliveryDeclarationBody,
+  ): Promise<TPostSaveDeliveryDeclarationResponse> {
+    const { body } = await this.makeRequest<IPostSaveDeliveryDeclarationBody, {}, TPostSaveDeliveryDeclarationResponse>(
+      HttpMethods.Post,
+      this.buildUrl('save_declaration_id'),
+      data,
+      {},
+    );
+
+    return body;
+  }
+
+  public static readonly BASE_PATH = 'delivery';
+}
