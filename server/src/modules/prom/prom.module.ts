27644d90a9a27824a002299647e9bf71974d4abc
Denys <zololotarenko.2015@gmail.com>
2022-08-13 17:56:25 +0300

    server(prom): update Prom module

--- server/src/modules/prom/prom.module.ts
+++ server/src/modules/prom/prom.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PromController } from './prom.controller';
 import { PromService } from './prom.service';
+import { PromProductsController } from './products/products.controller';
+import { PromProductsService } from './products/products.service';
 
 @Module({
   imports: [],
-  controllers: [PromController],
-  providers: [PromService],
-  exports: [],
+  controllers: [PromController, PromProductsController],
+  providers: [PromService, PromProductsService],
+  exports: [PromProductsService],
 })
 export class PromModule {}
