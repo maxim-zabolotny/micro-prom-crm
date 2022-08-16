19ee7a03c60f1712204ad0b61ccb638359ae1317
Denys <zololotarenko.2015@gmail.com>
2022-08-16 20:24:43 +0300

    server(job): add load-all-products consumer

--- server/src/modules/job/consumers/index.ts
+++ server/src/modules/job/consumers/index.ts
@@ -1,3 +1,4 @@
 export * from './audio.consumer';
 export * from './load-all-categories.consumer';
+export * from './load-all-products.consumer';
 export * from './load-products-by-category.consumer';
