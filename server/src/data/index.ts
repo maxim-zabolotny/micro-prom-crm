3ef9f9b36e4e127fda7579c68d94310f3f72080e
Denys <zololotarenko.2015@gmail.com>
2022-08-18 17:37:49 +0300

    server(data): update Data namespace

--- server/src/data/index.ts
+++ server/src/data/index.ts
@@ -20,6 +20,22 @@ export namespace Data {
     }
   }
 
+  export namespace SelectedRUCategories {
+    export const fileName = 'selected-ru-categories.json';
+    export const filePathDist = path.join(__dirname, fileName);
+    export const filePathSRC = filePathDist.replace('dist', 'src');
+
+    export async function read(): Promise<ICategory[]> {
+      const data = await fs.readFile(filePathSRC, { encoding: 'utf-8' });
+      return JSON.parse(data);
+    }
+
+    export async function write(categories: ICategory[]) {
+      const data = JSON.stringify(categories, null, 2);
+      await fs.writeFile(filePathSRC, data, { encoding: 'utf-8' });
+    }
+  }
+
   export namespace Users {
     export const fileName = 'users.json';
     export const filePathDist = path.join(__dirname, fileName);
