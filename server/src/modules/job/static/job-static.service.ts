00de3599af9238f46a72989b9813ea4ac630fe51
Denys <zololotarenko.2015@gmail.com>
2022-08-20 17:00:04 +0300

    server(job/static): implement JobStatic service

--- server/src/modules/job/static/job-static.service.ts
+++ server/src/modules/job/static/job-static.service.ts
@@ -1,16 +1,56 @@
+import * as _ from 'lodash';
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Queue } from 'bull';
 
 @Injectable()
 export class JobStaticService implements OnModuleInit {
   private readonly logger = new Logger(this.constructor.name);
 
+  private readonly staticQueues: Queue[] = [];
+
   constructor(private configService: ConfigService) {}
 
-  onModuleInit() {
-    // TODO: onModuleInit: recreate all static jobs
-    this.logger.error(
-      '----------------------------------------- MODULE INIT -------------------------------',
+  async onModuleInit() {
+    this.logger.log('Starting static jobs..');
+
+    await this.reCreateRepeatableJobs();
+
+    this.logger.log('Static jobs started');
+  }
+
+  public async reCreateRepeatableJobs() {
+    await Promise.all(
+      _.map(this.staticQueues, async (staticQueue) => {
+        const readableQueueName = staticQueue.name
+          .split('-')
+          .map((k) => _.capitalize(k))
+          .join(' ');
+
+        try {
+          const jobsToBeRemoved = await staticQueue.getRepeatableJobs();
+
+          if (jobsToBeRemoved.length) {
+            await Promise.all(
+              _.map(jobsToBeRemoved, (job) =>
+                staticQueue.removeRepeatableByKey(job.key),
+              ),
+            );
+
+            this.logger.debug(`"${readableQueueName}" old jobs removed:`, {
+              oldJobs: _.map(jobsToBeRemoved, 'key'),
+            });
+          }
+
+          await staticQueue.add({});
+          this.logger.debug(`"${readableQueueName}" started`);
+        } catch (err) {
+          this.logger.error('Received error until recreate repeatable jobs:', {
+            err,
+            queueName: readableQueueName,
+          });
+        }
+      }),
     );
   }
 }
