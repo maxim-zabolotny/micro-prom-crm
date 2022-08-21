49408add123a7d9d3029b4aad01aeec07e6ad690
Denys <zololotarenko.2015@gmail.com>
2022-08-21 22:09:36 +0300

    server(crm/users): update service

--- server/src/modules/crm/users/users.service.ts
+++ server/src/modules/crm/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from '@schemas/user';
+import { User, UserDocument, UserRole } from '@schemas/user';
 
 @Injectable()
 export class CrmUsersService {
@@ -12,4 +12,29 @@ export class CrmUsersService {
     private configService: ConfigService,
     @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
+
+  // MAIN
+  public getModel() {
+    return this.userModel;
+  }
+
+  public async getUserByRole(role: UserRole) {
+    return this.userModel.find({ role }).exec();
+  }
+
+  public async getAllUsers() {
+    return this.userModel.find().exec();
+  }
+
+  public async getAdmin() {
+    return this.getUserByRole(UserRole.Admin);
+  }
+
+  public async getProvider() {
+    return this.getUserByRole(UserRole.Provider);
+  }
+
+  public async getSales() {
+    return this.getUserByRole(UserRole.Sales);
+  }
 }
