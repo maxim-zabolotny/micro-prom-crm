559362cf5929f4471edb28d4e4ac5f6667f0a4f4
Denys <zololotarenko.2015@gmail.com>
2022-08-22 18:39:55 +0300

    server(databse/integration): add static methods

--- server/src/database/schemas/integration/integration.schema.ts
+++ server/src/database/schemas/integration/integration.schema.ts
@@ -1,8 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Model } from 'mongoose';
 import { IntegrationCompany } from '@schemas/integration/integration-company.enum';
+import { HttpException, HttpStatus, Logger } from '@nestjs/common';
 
+// CUSTOM TYPES
+type TStaticMethods = {
+  getMicrotronIntegration: (
+    this: IntegrationModel,
+  ) => Promise<IntegrationDocument>;
+};
+
+// MONGOOSE
 export type IntegrationDocument = Integration & Document;
+
+export type IntegrationModel = Model<IntegrationDocument> & TStaticMethods;
 
 @Schema({ timestamps: true, collection: 'integrations' })
 export class Integration {
@@ -16,3 +27,22 @@ export class Integration {
 }
 
 export const IntegrationSchema = SchemaFactory.createForClass(Integration);
+
+// STATIC METHODS IMPLEMENTATION
+const integrationLogger = new Logger('ConstantModel');
+
+IntegrationSchema.statics.getMicrotronIntegration = async function () {
+  integrationLogger.debug('Load Microtron Integration from DB');
+
+  const microtronIntegration = await this.findOne({
+    company: IntegrationCompany.Microtron,
+  }).exec();
+  if (!microtronIntegration) {
+    throw new HttpException(
+      'No saved microtron integration in DB',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+
+  return microtronIntegration;
+} as TStaticMethods['getMicrotronIntegration'];
