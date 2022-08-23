ead9bc9cdd56bfb5f48592003a89adf4132edd20
Denys <zololotarenko.2015@gmail.com>
2022-08-23 22:03:27 +0300

    server(crm/products): remove product model method-wrappers

--- server/src/modules/crm/products/types/add-product-to-db.type.ts
+++
@@ -1,6 +0,0 @@
-import { IProductFullInfo } from '@common/interfaces/product';
-import { CategoryDocument } from '@schemas/category';
-
-export type TAddProductToDB = Omit<IProductFullInfo, 'categoryId'> & {
-  category: Pick<CategoryDocument, '_id' | 'microtronId' | 'course' | 'markup'>;
-};
