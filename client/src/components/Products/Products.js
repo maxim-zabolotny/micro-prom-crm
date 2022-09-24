edc0416bb6b4c0288db7613ab6c80103918c4b63
Denys <zololotarenko.2015@gmail.com>
2022-09-24 12:02:19 +0300

    client(components/Products): add Products component

---
+++ client/src/components/Products/Products.js
@@ -0,0 +1,45 @@
+import { FindProducts } from "./FindProducts/FindProducts";
+import { useEffect, useState } from "react";
+import _ from "lodash";
+import { useAxios } from "../../hooks";
+import { API_URL } from "../../api/baseURL";
+import { Product } from "./Product/Product";
+
+export function Products() {
+  const [requestData, setRequestData] = useState({
+    offset: 0,
+    limit: 30,
+  });
+
+  const config = {
+    method: "post",
+    data: requestData,
+  };
+  const [data, error, loading, fetch] = useAxios(
+    API_URL.PRODUCTS.SEARCH,
+    config
+  );
+
+  useEffect(() => {
+    fetch();
+  }, [config.data]);
+
+  const products = _.isEmpty(data) ? null : (
+    <div>
+      {data.map((item) => (
+        <Product key={item._id} data={item} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div>
+      <FindProducts
+        data={requestData}
+        fetch={(data) => setRequestData(data)}
+        productsSize={(data ?? []).length}
+      />
+      {loading ? <p>LOADING..</p> : products}
+    </div>
+  );
+}
---
+++ client/src/components/Products/index.js
@@ -0,0 +1,2 @@
+export { Products } from "./Products";
+export { FindProducts } from "./FindProducts/FindProducts";
