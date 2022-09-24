ca13ce5fd1ab52fa5dc24d513e334d9ae2912da2
Denys <zololotarenko.2015@gmail.com>
2022-09-24 14:10:12 +0300

    client(pages): add Products page

---
+++ client/src/pages/products/index.js
@@ -0,0 +1,10 @@
+import { Main } from "../../components/Main";
+import { Products } from "../../components/Products";
+
+export function ProductsPage() {
+  return (
+    <Main>
+      <Products />
+    </Main>
+  );
+}
