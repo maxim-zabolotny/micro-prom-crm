ef7a6c2b215742beb00244e8b6d4a8e4fcf5365d
Denys <zololotarenko.2015@gmail.com>
2022-09-24 21:54:04 +0300

    client: add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,155 +1,53 @@
 /*external modules*/
-import _ from "lodash";
-import axios from "axios";
-import { Button, Tree } from "antd";
-import React, { useEffect, useState } from "react";
+import React from "react";
 /*utils*/
-import { makeTree } from "./utils/makeTree";
 /*styles*/
 import "./App.css";
+import { Main } from "./components/Main";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./pages/error";
+import { ProductsPage } from "./pages/products";
 
-const hostName = "192.168.3.18"; //'localhost'
+// export function Booking() {
+//   const loaderData = useLoaderData();
+//
+//   return (
+//     <>
+//       <div>Бронирование - {JSON.stringify(loaderData)}</div>
+//     </>
+//   );
+// }
 
-const API = {
-  host: `http://${hostName}:8080/microtron`,
-  async getCategories(tree = false) {
-    const { data } = await axios.get(`${this.host}/categories`, {
-      params: { tree },
-    });
-    return data;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/booking/*",
+        loader: ({ params }) => ({ id: params["*"] }),
+        element: <div>Бронирование</div>,
+      },
+      {
+        path: "/sale/*",
+        loader: ({ params }) => ({ id: params["*"] }),
+        element: <div>Продажи</div>,
+      },
+      {
+        path: "/client",
+        element: <div>Клиенты</div>,
+      },
+    ],
   },
-  async getSavedCategories(tree = false) {
-    const { data } = await axios.get(`${this.host}/categories/saved`, {
-      params: { tree },
-    });
-    return data;
-  },
-  async saveCategories(categories, isTree = false) {
-    const { data } = await axios.put(`${this.host}/categories/save`, {
-      categories,
-      isTree,
-    });
-    return data;
-  },
-  addKeyAndTitle(category) {
-    return {
-      ...category,
-      title: category.name,
-      key: category.id,
-    };
-  },
-  removeKeyAndTitle(category) {
-    return _.omit(category, ["title", "key"]);
-  },
-};
+]);
 
 function App() {
-  const [error, setError] = React.useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const [checkedKeys, setCheckedKeys] = useState([]);
-
-  const [categories, setCategories] = useState([]);
-  const [categoriesTree, setCategoriesTree] = useState([]);
-
-  const onCheck = (checkedKeysValue) => {
-    console.log("onCheck", checkedKeysValue);
-    setCheckedKeys(checkedKeysValue);
-  };
-
-  async function loadCategories(saved = true) {
-    setLoading(true);
-
-    try {
-      console.log("START -> loadCategories");
-      const rawData = saved
-        ? await API.getSavedCategories(false)
-        : await API.getCategories(false);
-
-      setCategories(rawData);
-
-      const tree = makeTree(rawData, "parentId", 0, API.addKeyAndTitle);
-      setCategoriesTree(tree);
-
-      console.log("END -> loadCategories");
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function save() {
-    setLoading(true);
-
-    try {
-      const result = await API.saveCategories(categories, false);
-      console.log("save result => ", result);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function loadFromAPI() {
-    await loadCategories(false);
-  }
-
-  async function loadSaved() {
-    await loadCategories(true);
-  }
-
-  function removeChecked() {
-    const updatedCategories = _.filter(
-      categories,
-      (category) => !_.includes(checkedKeys, category.id)
-    );
-
-    const tree = makeTree(updatedCategories, "parentId", 0, API.addKeyAndTitle);
-
-    setCategories(updatedCategories);
-    setCategoriesTree(tree);
-
-    setCheckedKeys([]);
-  }
-
-  useEffect(() => {
-    loadSaved();
-  }, []);
-
-  if (error) return `Error: ${error.message}`;
-
-  return (
-    <div className={"App"}>
-      <div className={"App-buttons"}>
-        <Button type="primary" loading={loading} onClick={save}>
-          Save
-        </Button>
-        <Button type="primary" loading={loading} onClick={loadFromAPI}>
-          Load from API
-        </Button>
-        <Button type="primary" loading={loading} onClick={loadSaved}>
-          Load saved
-        </Button>
-        <Button type="default" onClick={removeChecked}>
-          Remove checked
-        </Button>
-      </div>
-      <div className={"App-line"} />
-      {categories.length ? (
-        <Tree
-          checkable
-          autoExpandParent={false}
-          onCheck={onCheck}
-          checkedKeys={checkedKeys}
-          treeData={categoriesTree}
-        />
-      ) : (
-        <span style={{ color: "red" }}>No saved categories</span>
-      )}
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
