0b6201f1497cb2ff1e20c74cef5b6d526ff14fce
Denys <zololotarenko.2015@gmail.com>
2022-09-24 13:47:47 +0300

    server(telegram/common/helpers): update Markdown helper

--- server/src/modules/telegram/common/helpers/markdown.helper.ts
+++ server/src/modules/telegram/common/helpers/markdown.helper.ts
@@ -1,17 +1,26 @@
 /*external modules*/
+import { escapeMarkdown, md } from 'telegram-escape';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MarkdownHelper {
   static bold(text: string): string {
-    return `*${text}*`;
+    return md`*${text}*`;
   }
 
   static italic(text: string): string {
-    return `_${text}_`;
+    return md`_${text}_`;
   }
 
   static monospaced(text: string): string {
     return `\`${text}\``;
+  }
+
+  static escape(text: string | number): string {
+    return md`${text}`;
+  }
+
+  static json(text: string): string {
+    return escapeMarkdown('```json\n' + text + '\n```');
   }
 }
