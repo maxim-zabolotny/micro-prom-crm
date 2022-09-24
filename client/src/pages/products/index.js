5b4e34f7c0fcb4a981599943778ff5a06bb15448
Denys <zololotarenko.2015@gmail.com>
2022-09-24 21:03:09 +0300

    client(pages): update Products page

--- client/src/pages/products/index.js
+++ client/src/pages/products/index.js
@@ -1,10 +1,5 @@
-import { Main } from "../../components/Main";
 import { Products } from "../../components/Products";
 
 export function ProductsPage() {
-  return (
-    <Main>
-      <Products />
-    </Main>
-  );
+  return <Products />;
 }
