11f9eb8bd5fc4aea905b191f7e1784a8000cd367
Denys <zololotarenko.2015@gmail.com>
2022-09-24 13:08:45 +0300

    server(crm/productBookings): update CreateProductBooking DTO

--- server/src/modules/crm/productBookings/dto/create-product-booking.dto.ts
+++ server/src/modules/crm/productBookings/dto/create-product-booking.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 
 export class CreateProductBookingDto {
   @IsString()
@@ -6,6 +12,7 @@ export class CreateProductBookingDto {
   productId: string;
 
   @IsString()
+  @IsOptional()
   description = '';
 
   @IsNumber()
