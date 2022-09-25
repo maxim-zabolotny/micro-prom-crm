e1adc3bced535e8cda0ac41d18ef378334d27b6c
Denys <zololotarenko.2015@gmail.com>
2022-09-25 17:05:06 +0300

    client(pages): update Products page

--- client/src/pages/products/index.js
+++ client/src/pages/products/index.js
@@ -1,5 +1,53 @@
-import { Products } from "../../components/Products";
+import { FindProduct, Products } from "../../components/Products";
+import { useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { Button } from "antd";
 
 export function ProductsPage() {
-  return <Products />;
+  const loaderData = useLoaderData();
+
+  const [viewType, setViewType] = useState(
+    loaderData.id ? "single" : "multiple"
+  );
+
+  let children;
+  switch (viewType) {
+    case "single": {
+      children = <FindProduct productId={loaderData.id} />;
+      break;
+    }
+    case "multiple": {
+      children = <Products />;
+      break;
+    }
+    default: {
+      throw new Error("Непредвиденная ошибка");
+    }
+  }
+
+  const buttons = (
+    <div style={{ margin: "10px" }}>
+      <Button
+        style={{ marginRight: "10px" }}
+        type={viewType === "single" ? "primary" : "default"}
+        onClick={() => setViewType("single")}
+      >
+        Продукт
+      </Button>
+
+      <Button
+        type={viewType === "multiple" ? "primary" : "default"}
+        onClick={() => setViewType("multiple")}
+      >
+        Поиск
+      </Button>
+    </div>
+  );
+
+  return (
+    <div>
+      {buttons}
+      {children}
+    </div>
+  );
 }
