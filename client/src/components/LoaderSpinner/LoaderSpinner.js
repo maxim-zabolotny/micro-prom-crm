21b77109bea50e0dd0c3c21296432f80639c4b38
Denys <zololotarenko.2015@gmail.com>
2022-10-02 17:56:25 +0300

    client(components): add LoaderSpinner component

---
+++ client/src/components/LoaderSpinner/LoaderSpinner.js
@@ -0,0 +1,18 @@
+import { Oval } from "react-loader-spinner";
+import "./LoaderSpinner.css";
+
+export function LoaderSpinner({ width = 50, height = 50 }) {
+  return (
+    <div className={"loader-spinner"}>
+      <Oval
+        ariaLabel="loading-indicator"
+        height={height}
+        width={width}
+        strokeWidth={5}
+        strokeWidthSecondary={1}
+        color="blue"
+        secondaryColor="white"
+      />
+    </div>
+  );
+}
