ef1ac2184b12a9d3d8d1845e6154180a03c4d2a9
Denys <zololotarenko.2015@gmail.com>
2022-10-02 14:33:04 +0300

    client(components): update RequestAccess component

--- client/src/components/RequestAccess/RequestAccess.js
+++ client/src/components/RequestAccess/RequestAccess.js
@@ -1,14 +1,24 @@
 import { useRequestAccess } from "../../hooks";
 import React from "react";
 
-export function RequestAccess({ url, showMessage, children }) {
+export function RequestAccess({
+  url,
+  showWithoutModifyAccess,
+  showMessage,
+  children,
+}) {
   const [userHaveAccess, errorAccessMessage] = useRequestAccess(url);
 
   const message = showMessage ? errorAccessMessage : null;
   const clone = React.cloneElement(children, {
     ...children.props,
     url,
+    haveAccess: userHaveAccess,
   });
+
+  if (showWithoutModifyAccess) {
+    return clone;
+  }
 
   return userHaveAccess ? clone : message;
 }
