03311ea148b3b97997ad01f088cb66735484e64d
Denys <zololotarenko.2015@gmail.com>
2022-10-02 20:45:17 +0300

    server(common/interfaces): update telegram send-notification interface

--- server/src/common/interfaces/telegram/send-notification.interface.ts
+++ server/src/common/interfaces/telegram/send-notification.interface.ts
@@ -1,6 +1,7 @@
 export interface ISendNotification {
   to: string;
   title: string;
+  photo?: string;
   jsonObject?: Record<string, unknown>;
   details?: Array<[string, string | number]>;
   buttons?: Array<[string, string]>;
