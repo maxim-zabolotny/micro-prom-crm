7c34223e274ec4a1610ac0576605c7b86300c300
Denys <zololotarenko.2015@gmail.com>
2022-10-02 20:09:03 +0300

    client(components/Navigation): add styles

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,20 +1,16 @@
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 export function Navigation() {
   return (
-    <div
-      style={{
-        height: "100%",
-        width: "300px",
-        backgroundColor: "aqua",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Link to={`/`}>Продукты</Link>
-      <Link to={`/booking`}>Бронирование</Link>
-      <Link to={`/sale`}>Продажи</Link>
-      <Link to={`/client`}>Клиенты</Link>
+    <div className={"navigation"}>
+      <div className={"navigation-container"}>
+        <Link to={`/`}>Продукты</Link>
+        <Link to={`/booking`}>Бронирование</Link>
+        <Link to={`/sale`}>Продажи</Link>
+        <Link to={`/client`}>Клиенты</Link>
+      </div>
+      <div className={"navigation-line"} />
     </div>
   );
 }
