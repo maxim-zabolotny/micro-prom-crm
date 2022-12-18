777c3456b5f8e46fc8a6b40747198110ae8d9711
Denys <dzolo@softserveinc.com>
2022-12-18 20:10:06 +0200

    server(microtron/courses): add tolerance for cache

--- server/src/modules/microtron/courses/courses.service.ts
+++ server/src/modules/microtron/courses/courses.service.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import * as ms from 'ms';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import MicrotronAPI, { Course } from '@lib/microtron';
@@ -10,7 +11,10 @@ export class MicrotronCoursesService {
   private readonly logger = new Logger(this.constructor.name);
 
   private readonly coursesAPI: Course.Course;
+
   private coursesCache: ICourse | null = null;
+  private coursesLastRequestTimestamp: number | null = null;
+  public coursesRequestTolerance: number = ms('5m');
 
   constructor(private configService: ConfigService) {
     this.coursesAPI = new MicrotronAPI.Course({
@@ -26,18 +30,33 @@ export class MicrotronCoursesService {
       courses,
     });
 
+    this.coursesLastRequestTimestamp = Date.now();
     return courses;
   }
 
-  public async getCoursesByAPI(force: boolean): Promise<ICourse> {
+  public setRequestTolerance(tolerance: number) {
+    this.coursesRequestTolerance = tolerance;
+  }
+
+  public async getCoursesByAPI(
+    force: boolean,
+    tolerance = this.coursesRequestTolerance,
+  ): Promise<ICourse> {
     const cacheIsEmpty = _.isEmpty(this.coursesCache);
 
     this.logger.debug('Load courses from API', {
       force,
       cacheIsEmpty,
+      tolerance,
     });
 
-    if (force || cacheIsEmpty) {
+    if (cacheIsEmpty) {
+      return this.retrieveCoursesFromAPI();
+    }
+
+    const cacheIsExpired =
+      Date.now() - this.coursesLastRequestTimestamp >= tolerance;
+    if (force && cacheIsExpired) {
       return this.retrieveCoursesFromAPI();
     }
 
